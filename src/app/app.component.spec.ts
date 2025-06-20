4e5595cd50c5f44f1e8a76eeaa22ce300365df06
sergiocastro85 <sergiocastro86@gmail.com>
2025-06-19 20:04:26 -0500

    fix: update project name and adjust title expectations in tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,19 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-portfolio' title`, () => {
+  it(`should have the 'my-portfolio
+' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-portfolio');
+    expect(app.title).toEqual('my-portfolio
+');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-portfolio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-portfolio
+');
   });
 });
