5473e0941adcc178befc2abe6bd31c2a3dc77d80
sergiocastro85 <sergiocastro86@gmail.com>
2025-06-27 15:18:26 -0500

    feat: add mobile menu functionality and deploy configuration

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  imports: [CommonModule],
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
 }
